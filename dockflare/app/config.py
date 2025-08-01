18ee2325d1686b5945bdf984b10529bb06030992
ChrispyBacon-dev <ori@hotmail.de>
2025-08-01 14:36:53 +0200

    update Readme and version

--- dockflare/app/config.py
+++ dockflare/app/config.py
@@ -25,7 +25,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # --- DockFlare Version ---
-APP_VERSION = "v1.9.6"
+APP_VERSION = "v2.0.0"
 # --- web: https://dockflare.app ---
 # --- github: https://github.com/ChrispyBacon-dev/DockFlare ---
 
